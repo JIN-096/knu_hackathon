b614bb97cc94d85e81e465b9867d405c62d8f1c2
cheonjiwan <giyan1000@naver.com>
2021-07-19 21:49:39 +0900

    Set StartActivity

--- app/src/main/java/com/example/knuhackthon/SignInActivity.kt
+++ app/src/main/java/com/example/knuhackthon/SignInActivity.kt
@@ -1,11 +1,21 @@
 package com.example.knuhackthon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.knuhackthon.databinding.ActivitySignInBinding
 
 class SignInActivity : AppCompatActivity() {
+
+    val binding by lazy { ActivitySignInBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_in)
+        setContentView(binding.root)
+
+        binding.btnSignin.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
