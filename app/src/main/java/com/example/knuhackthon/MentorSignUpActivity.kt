8bde436c1820a0447f9f091ad7f5b5c570709daf
cheonjiwan <giyan1000@naver.com>
2021-07-20 18:13:34 +0900

    signup part1

--- app/src/main/java/com/example/knuhackthon/MentorSignUpActivity.kt
+++ app/src/main/java/com/example/knuhackthon/MentorSignUpActivity.kt
@@ -1,30 +1,40 @@
 package com.example.knuhackthon
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.knuhackthon.databinding.ActivityMentorSignUpBinding
-import com.example.knuhackthon.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.ActionCodeSettings
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.util.*
+import javax.mail.Message
+import javax.mail.PasswordAuthentication
+import javax.mail.Session
+import javax.mail.Transport
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 class MentorSignUpActivity : AppCompatActivity() {
 
     val binding by lazy { ActivityMentorSignUpBinding.inflate(layoutInflater) }
     var auth : FirebaseAuth? = null
+    var email : String? = null
+    var pw : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         auth = FirebaseAuth.getInstance()
-
+        binding.btnEmailVerify.setOnClickListener {
+            email = binding.emailText.text.toString()!!
+            var code = createEmailCode()
+            CoroutineScope(Dispatchers.IO).launch { sendEmail("인증 메일입니다",code!!,email!!) }
+        }
         binding.btnSignup.setOnClickListener { signUp() }
-    }
-
-    override fun onStart() {
-        super.onStart()
-        moveMainPage(auth?.currentUser)
     }
 
     fun signUp(){
@@ -42,7 +52,15 @@ class MentorSignUpActivity : AppCompatActivity() {
     }
 
     fun sendVerificationEmail(){
-        var user = auth?.currentUser
+        var user = auth?.currentUser!!
+
+        val url = "https://knuhackthon.page.link/check"
+        val actionCodeSettings = ActionCodeSettings.newBuilder()
+            .setUrl(url)
+            .setIOSBundleId("com.example.ios")
+            .setAndroidPackageName("com.example.knuhackthon",false,null)
+            .build()
+
         user?.sendEmailVerification()?.addOnCompleteListener {
             task ->
             if(task.isSuccessful){
@@ -62,10 +80,64 @@ class MentorSignUpActivity : AppCompatActivity() {
         Toast.makeText(this,"계정 생성 성공. 검증용 이메일 전송",Toast.LENGTH_LONG).show()
     }
 
-    fun moveMainPage(user: FirebaseUser?){
-        if(user!=null){
-            startActivity(Intent(this,MainActivity::class.java))
-            finish()
+    suspend fun sendEmail(
+        title: String,      // 메일 제목
+        body: String,       // 메일 내용
+        dest: String,       // 받는 메일 주소
+    )
+    {
+        // 보내는 메일 주소와 비밀번호
+        val username = "";
+        val password = "";
+
+        val props = Properties();
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        // 비밀번호 인증으로 세션 생성
+        val session = Session.getInstance(props,
+            object: javax.mail.Authenticator() {
+                override  fun getPasswordAuthentication(): PasswordAuthentication {
+                    return PasswordAuthentication(username, password);
+                }
+            })
+
+        // 메시지 객체 만들기
+        val message = MimeMessage(session)
+        message.setFrom(InternetAddress(username))
+        // 수신자 설정, 여러명으로도 가능
+        message.setRecipients(
+            Message.RecipientType.TO,
+            InternetAddress.parse(dest))
+        message.subject = title
+        message.setText(body)
+
+
+        /*// 파일을 담기 위한 Multipart 생성
+        val multipart = MimeMultipart()
+        val messageBodyPart = MimeBodyPart()
+        val source = FileDataSource(filePath)
+
+        messageBodyPart.dataHandler = DataHandler(source)
+        messageBodyPart.fileName = fileName
+        multipart.addBodyPart(messageBodyPart)
+
+        // 메시지에 파일 담고
+        message.setContent(multipart)*/
+
+        // 전송
+        Transport.send(message)
+    }
+
+     fun createEmailCode(): String? { //이메일 인증코드 생성
+        val str = arrayOf("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","1","2","3","4","5","6","7","8","9")
+        var newCode = String()
+        for (x in 0..7) {
+            val random = (Math.random() * str.size).toInt()
+            newCode += str[random]
         }
+        return newCode
     }
 }
--- app/src/main/java/com/example/knuhackthon/SignInActivity.kt
+++ app/src/main/java/com/example/knuhackthon/SignInActivity.kt
@@ -3,6 +3,7 @@ package com.example.knuhackthon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.knuhackthon.databinding.ActivitySignInBinding
 import com.google.firebase.auth.FirebaseAuth
 
@@ -21,14 +22,25 @@ class SignInActivity : AppCompatActivity() {
         email = intent.getStringExtra("email")
         pw = intent.getStringExtra("pw")
 
-
         binding.btnSignin.setOnClickListener {
             auth?.signInWithEmailAndPassword(email!!,pw!!)
                 ?.addOnCompleteListener {
                     task ->
                     if(task.isSuccessful){
+                        /*if(isEmailVerified()){
+                            val intent = Intent(this, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+                        else{
+                            auth?.signOut()
+                            Toast.makeText(this,"인증메일을 확인해주세요",Toast.LENGTH_LONG).show()
+                        }*/
+
                         val intent = Intent(this, MainActivity::class.java)
                         startActivity(intent)
+                    }
+                    else{
+                        Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
                     }
                 }
         }
@@ -38,4 +50,11 @@ class SignInActivity : AppCompatActivity() {
             startActivity(intent)
         }
     }
+
+    /*fun isEmailVerified(){
+        var user = auth?.currentUser
+        if(user!=null){
+            var accessToken =
+        }
+    }*/
 }
