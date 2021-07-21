9f9273c114e1edb04f3a3cbad815460cdad17e81
천지완 <giyan1000@gmail.com>
2021-07-21 15:10:29 +0900

    Set bottomnavigation & board ui

---
+++ app/src/main/java/com/example/knuhackthon/navigation/board/BoardItem.kt
@@ -0,0 +1,9 @@
+package com.example.knuhackthon.navigation.board
+
+data class BoardItem(
+    val author: String,
+    val title: String,
+    val content: String,
+    val date : String,
+    val CommentCnt : String
+)
