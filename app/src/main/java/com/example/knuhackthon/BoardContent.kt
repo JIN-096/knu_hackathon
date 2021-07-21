0e24024738f2b9e00a6599322a8501bc79611502
JIN-096 <leejeongjin1@gmail.com>
2021-07-22 00:13:34 +0900

    board update

---
+++ app/src/main/java/com/example/knuhackthon/BoardContent.kt
@@ -0,0 +1,11 @@
+package com.example.knuhackthon.navigation.board
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BoardContent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_content)
+    }
+}
